0f00d7a604c59a0accb9055b5c9307d73062ae4d
Hyok S. Choi <hyok.choi@samsung.com>
2023-08-21 07:35:08 +0000

    debug capital/{country}

--- server.go
+++ server.go
@@ -59,6 +59,36 @@ func main() {
 	rtr.HandleFunc("/servertime", getServerTime)
 	rtr.HandleFunc("/random", getRandomNumber)
 	rtr.HandleFunc("/users/{category}/{id:[0-9]+}", handleUsers)
+	rtr.HandleFunc("/capital/{country}", home)
+	err := rtr.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		pathTemplate, err := route.GetPathTemplate()
+		if err == nil {
+			fmt.Println("ROUTE:", pathTemplate)
+		}
+		pathRegexp, err := route.GetPathRegexp()
+		if err == nil {
+			fmt.Println("Path regexp:", pathRegexp)
+		}
+		queriesTemplates, err := route.GetQueriesTemplates()
+		if err == nil {
+			fmt.Println("Queries templates:", strings.Join(queriesTemplates, ","))
+		}
+		queriesRegexps, err := route.GetQueriesRegexp()
+		if err == nil {
+			fmt.Println("Queries regexps:", strings.Join(queriesRegexps, ","))
+		}
+		methods, err := route.GetMethods()
+		if err == nil {
+			fmt.Println("Methods:", strings.Join(methods, ","))
+		}
+		fmt.Println()
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	fmt.Println("Server listening on port " + port)
 	log.Fatal(http.ListenAndServe(port, rtr))
 }
